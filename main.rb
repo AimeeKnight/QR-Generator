04db94e5f3e7d9412332c259273a8303364a6b9e
Aimee Knight <AimeeKnight83@gmail.com>
2013-10-22 12:38:03 -0400

    add submit form and post action for displaying pdf in browser

--- main.rb
+++ main.rb
@@ -3,36 +3,25 @@ require 'json'
 require 'net/http'
 require 'sinatra'
 require 'sinatra/flash'
+require 'prawn'
 require 'prawn/qrcode'
 require './env' if File.exists?('env.rb')
 enable :sessions
 
 get '/' do    
-    erb :index  
+	erb :index
 end  
 
 post '/' do
-	# qrcode_content = "http://github.com/jabbrwcky/prawn-qrcode"
-	# qrcode = RQRCode::QRCode.new("http://github.com/jabbrwcky/prawn-qrcode", :level=>:h, :size => 5)
+	qrcode_content = params[url].to_s
+	qrcode = RQRCode::QRCode.new(qrcode_content, :level=>:h, :size => 5)
 
-	# # Render a prepared QRCode at he cursor position
-	# # using a default module (e.g. dot) size of 1pt or 1/72 in
-	# Prawn::Document::new do
-	#   render_qr_code(qrcode)
-	#   render_file("qr1.pdf")
-	# end
-
-	# # Render a code for raw content and a given total code size.
-	# # Renders a QR Code at the cursor position measuring 1 in in width/height.
-	# Prawn::Document::new do
-	#   print_qr_code(qrcode_content, :extent=>72)
-	#   render_file("qr2.pdf")
-	# end
-
-	# # Render a code for raw content with a given dot size
-	# Prawn::Document::new do
-	#   # Renders a QR Code at he cursor position using a dot (module) size of 2.8/72 in (roughly 1 mm).
-	#   render_qr_code(qrcode_content, :dot=>2.8)
-	#   render_file("qr3.pdf")
-	# end
+	# Render a prepared QRCode at he cursor position
+	# using a default module (e.g. dot) size of 1pt or 1/72 in
+	@pdf = Prawn::Document::new do
+	  render_qr_code(qrcode)
+	  render_file("qr1.pdf")
+	end		
+	content_type 'application/pdf'
+	@pdf.render
 end
