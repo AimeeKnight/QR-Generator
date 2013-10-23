f46a1d17acf25196aaeeb92f66f2bfbe94506c77
Aimee Knight <AimeeKnight83@gmail.com>
2013-10-23 18:46:49 -0400

    add flash error for invalid URL

--- main.rb
+++ main.rb
@@ -14,15 +14,23 @@ end
 
 post '/' do
 	qrcode_content = params[:url].to_s
-	puts qrcode_content
-	qrcode = RQRCode::QRCode.new(qrcode_content, :level=>:h, :size => 5)
+	if valid_url qrcode_content
+		qrcode = RQRCode::QRCode.new(qrcode_content, :level=>:h, :size => 5)
 
-	# Render a prepared QRCode at he cursor position
-	# using a default module (e.g. dot) size of 1pt or 1/72 in
-	@pdf = Prawn::Document::new do
-	  render_qr_code(qrcode)
-	  render_file("qr1.pdf")
-	end		
-	content_type 'application/pdf'
-	@pdf.render
+		# Render a prepared QRCode at he cursor position
+		# using a default module (e.g. dot) size of 1pt or 1/72 in
+		@pdf = Prawn::Document::new do
+		  render_qr_code(qrcode)
+		  render_file("qr1.pdf")
+		end		
+		content_type 'application/pdf'
+		@pdf.render
+	else
+		flash.now[:error] = "Please enter a valid URL."
+		erb :index
+	end
+end
+
+def valid_url url
+	url =~ /https?:\/\/[\S]+/
 end
