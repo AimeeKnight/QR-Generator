5c02807d225020cab7022924a8d12134f3ae1227
Aimee Knight <AimeeKnight83@gmail.com>
2014-01-06 13:10:57 -0600

    add options for qr code

--- main.rb
+++ main.rb
@@ -15,7 +15,7 @@ end
 post '/' do
 	qrcode_content = params[:url].to_s
 	if valid_url qrcode_content
-		qrcode = RQRCode::QRCode.new(qrcode_content, :level=>:h, :size => 5)
+		qrcode = RQRCode::QRCode.new(qrcode_content, :level=>:h, :size => 5, :extend=> 72)
 
 		# Render a prepared QRCode at he cursor position
 		# using a default module (e.g. dot) size of 1pt or 1/72 in
